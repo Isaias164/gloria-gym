eb4ce96d1dd29f7fdddeca889c707fce39d28dca
Isaias <sosaisaias250@gmail.com>
2022-02-06 11:06:47 -0300

    agregeue vue a algunos archivos

---
+++ gloriaGym/front/js/main.js
@@ -0,0 +1,9 @@
+const confirmar_recerba = new Vue({
+    el: "#confirmacion_recerba",
+    data: {
+        succes: "./img/cara triste.png",
+        failed: "./img/cara feliz.png",
+        message: "",
+        recerba_hecha: true
+    }
+})
